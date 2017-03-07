8575e447a82bde947b5c3040c293c5699fd1b051
Robb Nicholson <robb.nicholson@gm.com>
2017-03-07 12:41:35 -0700

    serving static files

--- src/main/java/ran488/ratpack/Main.java
+++ src/main/java/ran488/ratpack/Main.java
@@ -1,12 +1,21 @@
 package ran488.ratpack;
 
 import ratpack.server.RatpackServer;
+import ratpack.server.ServerConfig;
 
+/**
+ * To run from command line using Gradle: gradlew run
+ *
+ */
 public class Main {
 	public static void main(String... args) throws Exception {
-		RatpackServer.start(server -> server.handlers(chain -> chain.
-				get(ctx -> ctx.render("Hello World!")).
-				get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
+		RatpackServer.start(server -> server
+			.serverConfig(ServerConfig.builder().findBaseDir().port(5000).build())
+			.registryOf(registry -> registry.add("World! (from registry)")) 
+			.handlers(chain -> chain
+				.get(ctx -> ctx.render("Hello " + ctx.get(String.class))) 							// http://localhost:5000
+				.files() 																			// http://localhost:5000/test-file.txt
+				.get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!")) 	// http://localhost:5000/Robb
 			)
 		);
 	}
